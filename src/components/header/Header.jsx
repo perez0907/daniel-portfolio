ac4c42d4ccaee75ce274e347f26bdcc2218ca2cd
perez0907 <danielalejandroperez018@gmail.com>
2025-03-04 23:18:47 +0900

    changed head

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
     <header id="home">
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Rasif Taghizade</h1>
-        <h5 className="text-light">Front-end Developer</h5>
+        <h1>Daniel Alejandro Perez Victoria</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
           Scroll Down
